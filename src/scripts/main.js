7f4a39ffc7467cd1eff8f6a5d977a01d862a7e66
TiFAu <turchin.anton2@gmail.com>
2026-02-24 22:51:09 +0200

    added first solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+function addingPlaceholderAndLabel(element, index, arr) {
+  const elementLabel = document.createElement('label');
+
+  element.placeholder =
+    element.name.charAt(0).toUpperCase() + element.name.slice(1);
+
+  elementLabel.className = 'field-label';
+  elementLabel.htmlFor = element.id;
+  elementLabel.textContent = element.name.toUpperCase();
+  element.parentElement.prepend(elementLabel);
+}
+
+const allInput = [...document.querySelectorAll('.field-text')];
+
+allInput.forEach(addingPlaceholderAndLabel);
