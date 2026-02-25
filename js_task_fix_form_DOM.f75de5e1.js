f14809eb0afaba4b64f7f80b5ceb7476c225c538
TiFAu <turchin.anton2@gmail.com>
2026-02-25 20:07:54 +0200

    make build

---
+++ js_task_fix_form_DOM.f75de5e1.js
@@ -0,0 +1,16 @@
+'use strict';
+function addingPlaceholderAndLabel(element, index, arr) {
+    const elementLabel = document.createElement('label');
+    if (element.name) element.placeholder = element.name.charAt(0).toUpperCase() + element.name.slice(1);
+    elementLabel.className = 'field-label';
+    if (!element.id && element.name) elementLabel.htmlFor = element.name;
+    elementLabel.htmlFor = element.id;
+    elementLabel.textContent = element.name.toUpperCase();
+    element.parentElement.prepend(elementLabel);
+}
+const allInputs = [
+    ...document.querySelectorAll('form input')
+];
+allInputs.forEach(addingPlaceholderAndLabel);
+
+//# sourceMappingURL=js_task_fix_form_DOM.f75de5e1.js.map
